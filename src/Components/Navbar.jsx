ae7978c48975ee3c0770141489f95b5ebd4c5060
Akshith17323 <akhi171356@gmail.com>
2025-05-07 21:01:06 +0530

    Added animation

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,10 +9,10 @@ function Navbar() {
           <div className='flex bg-slate-900 items-center justify-between  w-[90vw] h-12 p-2.5 rounded-lg logo-animate'>
             <a>Logo</a>
             <nav className='w-1/3 flex justify-between text-1.5xl text-white'>
-              <a id='linkItems' className='nav-animate'>Home</a>
-              <a id='linkItems' className='nav-animate'>About Us</a>
-              <a id='linkItems' className='nav-animate'>Wishlist</a>
-              <button className='bg-red-900 rounded-lg h-8 w-1/6  '>About</button>
+              <a id='linkItems' className='nav-animate' >Home</a>
+              <a id='linkItems' className='nav-animate' >About Us</a>
+              <a id='linkItems' className='nav-animate' >Wishlist</a>
+              <button className='bg-red-900 rounded-lg h-8 w-1/6  '>About Us</button>
             </nav>
           </div>
       </div>
